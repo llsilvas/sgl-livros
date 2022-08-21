50db063329267b3d1eb9fd8da03683eab5eb9a3c
Leandro <leandroasilva@apoioprodesp.sp.gov.br>
2022-08-20 21:38:05 -0300

    initial commit

---
+++ src/main/java/br/dev/sgl/livros/SglLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.dev.sgl.livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SglLivrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SglLivrosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/dev/sgl/livros/SglLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.sgl.livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SglLivrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
